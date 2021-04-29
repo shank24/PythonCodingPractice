b7166dcc3ede9ba22bdc6edd4ea454fc27e49bfd
shankkalra <shankkalra@gmail.com>
2021-04-29 07:58:59 +0530

    Time complexity

--- geeksforgeeks/analysis_of_algorithm/problemsInSession/Sum_First_Natural_Numbers.py
+++ geeksforgeeks/analysis_of_algorithm/problemsInSession/Sum_First_Natural_Numbers.py
@@ -7,7 +7,7 @@ def fun1(n):
     if n == 0 or n < 0:
         return 0
     return int(n * (n + 1) / 2)
-
+#Time Taken - C1 (Constant)
 
 def fun2(n):
     if n == 0 or n < 0:
@@ -16,7 +16,7 @@ def fun2(n):
     for i in range(1, n + 1):
         sum += i
     return sum
-
+#Time Taken - C2*n + C3
 
 def fun3(n):
     if n == 0 or n < 0:
@@ -26,7 +26,7 @@ def fun3(n):
         for j in range(1, i + 1):
             sum += 1
     return sum
-
+#Time Taken - C4*n2 + C5*n + C6
 
 print(fun1(5))
 print(fun2(5))
