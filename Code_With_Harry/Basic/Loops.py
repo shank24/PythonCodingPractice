24ff53a7f283175fb57d3087e4024d6f2ea96459
shankkalra <shankkalra@gmail.com>
2021-08-05 11:31:53 +0530

    Loops Code

---
+++ Code_With_Harry/Basic/Loops.py
@@ -0,0 +1,22 @@
+
+list1 = [["Harry",1], ["Carry",2] ,
+         ["Marry",3], ["Larry",4]]
+
+dict1 = dict(list1)
+print(dict1)
+
+#Basic list
+for l,value in list1:
+    print(l,value)
+
+#With Dictionary
+for l, value in dict1.items():
+    print (l, value)
+
+#Only keys
+for l in dict1:
+    print (l)
+
+#Only Values
+for l in dict1.values():
+    print (l)
