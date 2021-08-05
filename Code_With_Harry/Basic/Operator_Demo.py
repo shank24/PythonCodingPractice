e1d24c9ef5a1a863fc9692cce3cc6477fac581fa
shankkalra <shankkalra@gmail.com>
2021-08-05 15:55:46 +0530

    Operators

--- Code_With_Harry/Basic/Operator_Demo.py
+++ Code_With_Harry/Basic/Operator_Demo.py
@@ -20,8 +20,8 @@ def computeValue(a,b,op):
     elif op == '%':
         return a % b;    
 
-#value = computeValue(a,b,op)
-#print(value)
+value = computeValue(a,b,op)
+print(value)
 
 #Identity Operator
 print(5 is not 7)
