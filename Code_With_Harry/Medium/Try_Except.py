daf623344d2c6df9d55aa55091bfda2ca42dd0cb
shankkalra <shankkalra@gmail.com>
2021-08-07 18:21:38 +0530

    Adding Try Code

--- Code_With_Harry/Medium/Try_Except.py
+++ Code_With_Harry/Medium/Try_Except.py
@@ -1,6 +1,6 @@
 #Exception demonstration
-num1 = input()
-num2 = input()
+num1 = raw_input()
+num2 = raw_input()
 
 try:
     print("The sum is", int(num1)+int(num2))
@@ -9,3 +9,8 @@ except Exception as e:
     
 
 print("Execution resumes")
+
+Itemscart = 0
+
+if Itemscart!=2:
+    raise Exception("Itemscart Index Out of range")
