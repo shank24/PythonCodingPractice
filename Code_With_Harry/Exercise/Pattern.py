7219a8c1129e6207b1e5fd6425d14c5681fc4f83
shankkalra <shankkalra@gmail.com>
2021-08-07 17:07:27 +0530

    Adding Pattern Code

---
+++ Code_With_Harry/Exercise/Pattern.py
@@ -0,0 +1,21 @@
+#Pattern Printing
+
+one = int(input('Enter a Row'))
+
+value = input('Type 1 or 0: ')
+two = bool(value)
+
+
+
+if two == True:
+    for i in range(1,one+1):
+        for j in range(1,i+1):
+            print("*")
+        print()
+elif two == False:
+    for i in range(one,0,-1):
+        for j in range(1,i+1):
+            print("*")
+        print()
+
+
--- Code_With_Harry/Medium/File_Writing.py
+++ Code_With_Harry/Medium/File_Writing.py
@@ -2,6 +2,7 @@ f = open("shank1.txt","a")
 a = f.write("Learning Python\n")
 print(a)
 
+#Handle Read & Write Both
 f = open("/home/shanky/Personal/Code/VS_Code_Python/shank1.txt","r+")
 print(f.read())
 f.write("Thanks!!")
