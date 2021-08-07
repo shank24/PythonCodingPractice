dfc313389cbd63fa4ad7d1fbc0d87136917ba806
shankkalra <shankkalra@gmail.com>
2021-08-07 14:43:11 +0530

    File IO

---
+++ Code_With_Harry/Medium/Inhertiance.py
@@ -0,0 +1,17 @@
+
+from Calc import Calculator
+
+
+class ChildImpl(Calculator):
+    num2 = 200
+    def __init__(self):
+        Calculator.__init__(self,2,9)
+
+    def getCompleteData(self):
+        return self.num2 + self.num + self.Summation()
+
+
+ob = ChildImpl()
+print(ob.getCompleteData())
+
+
