d8f7dddbc4b726ede17fdcc59a6fb1bc03bfc621
shankkalra <shankkalra@gmail.com>
2021-08-09 15:38:25 +0530

    Global Variables

--- Code_With_Harry/Medium/Global_Var.py
+++ Code_With_Harry/Medium/Global_Var.py
@@ -11,3 +11,16 @@ def function(n):
     print(n,"Via Function")
 
 function("Calling function")
+
+
+#Global always look out for out of function declarations.
+def fun1():
+    x=20
+    def fun2():
+        global x
+        x=90
+    print("Before calling fun2()",x)
+    fun2()
+    print("After calling fun2()",x)
+        
+fun1()
