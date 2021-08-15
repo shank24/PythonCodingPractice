0e8ea5e9d39b642354a737750289f60049dab5b7
shankkalra <shankkalra@gmail.com>
2021-08-15 17:14:54 +0530

    Adding Recursive Code

--- Code_With_Harry/Medium/Recursion.py
+++ Code_With_Harry/Medium/Recursion.py
@@ -3,19 +3,32 @@ def fun1(str):
 
 fun1("Mike Ross") 
 
+"""Recursive - Way"""
 def factorial(n):
-    if(n==0 | n==1):
+    if(n==1 or n==0):
         return 1
     else:
-        print(factorial(n)*factorial(n-1))
+        return n*factorial(n-1)
 
-#factorial(5)
+print(factorial(5))
 
-
+"""Iterative - Way"""
 def fact(n):
     val=1
     for i in range(n):
         val = val*(i+1)
     return val
 
-print(fact(5))
+print(fact(7))
+
+#Fibbonacci Series
+def fibbonacci(n):
+    if n==1:
+        return 0
+    elif n==2:
+        return 1
+    else:
+        return fibbonacci(n-1) + fibbonacci(n-2)
+    
+print(fibbonacci(7))
+
