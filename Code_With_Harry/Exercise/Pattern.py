d9d0fbfebfa7502cde6d05ca8524d9eac9fd9aa6
shankkalra <shankkalra@gmail.com>
2021-08-15 17:16:41 +0530

    Pattern Code

--- Code_With_Harry/Exercise/Pattern.py
+++ Code_With_Harry/Exercise/Pattern.py
@@ -1,12 +1,11 @@
 #Pattern Printing
 
 one = int(input('Enter a Row'))
-
 value = input('Type 1 or 0: ')
 two = bool(value)
 
 
-
+#Solution
 if two == True:
     for i in range(1,one+1):
         for j in range(1,i+1):
