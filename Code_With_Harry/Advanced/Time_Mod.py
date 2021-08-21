e4a9357d97af049035fd67b049ac60f1784b1bc5
shankkalra <shankkalra@gmail.com>
2021-08-21 09:44:53 +0530

    Loca Time

--- Code_With_Harry/Advanced/Time_Mod.py
+++ Code_With_Harry/Advanced/Time_Mod.py
@@ -7,10 +7,13 @@ start_time = time.time()
 while k<5:
     print("Shank 123")
     k+=1
-print(time.time() -start_time)
+print("While Loop Ran In ", time.time() -start_time, "Seconds")
 
 start_time1 = time.time()
 for i in range(5):
     print("Shank")
 
-print(time.time() -start_time1)
+print("For Loop Ran In ", time.time() -start_time1, "Seconds")
+
+localtime = time.asctime(time.localtime(time.time()))
+print(localtime)
