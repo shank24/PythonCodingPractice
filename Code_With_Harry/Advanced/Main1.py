3f4253ecbc8303c3553228d9fab5d8068c44f78e
shankkalra <shankkalra@gmail.com>
2021-08-31 11:36:31 +0530

    Main Demo

--- Code_With_Harry/Advanced/Main1.py
+++ Code_With_Harry/Advanced/Main1.py
@@ -5,6 +5,9 @@ def printshank(string):
 def add(num1,num2):
     return num1 + num2 + 5
 
-print(printshank("Testing String"))
-val = add(4,7)
-print(val)
+print("Name" , __name__)
+if __name__ == "__main__":
+
+    print(printshank("Testing String"))
+    val = add(4,7)
+    print(val)
