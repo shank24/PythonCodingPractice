ecc567d8852ab0ef27458ef326230f8dba796d4d
shankkalra <shankkalra@gmail.com>
2021-08-31 11:14:36 +0530

    Main func

---
+++ Code_With_Harry/Advanced/Main1.py
@@ -0,0 +1,10 @@
+
+def printshank(string):
+    return "This is the string demo - " + string
+
+def add(num1,num2):
+    return num1 + num2 + 5
+
+print(printshank("Testing String"))
+val = add(4,7)
+print(val)
