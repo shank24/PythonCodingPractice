872fbb925fee7ba0287923ae6875f87c289e2c87
shankkalra <shankkalra@gmail.com>
2021-09-01 12:43:18 +0530

    Map Func demo

--- Code_With_Harry/Advanced/Map_Filter.py
+++ Code_With_Harry/Advanced/Map_Filter.py
@@ -11,17 +11,34 @@ number = list(map(int, numbers))
 
 print(number)
 
-def squ(a):
-    return a*a
+# def squ(a):
+#     return a*a
     
+# num = [1,2,3,4,5,6,7,8,9,10]
+# square = list(map(squ, num))
+
+# print(square)
+
+
+# #lambda Function
+# num = [1,2,3,4,5,6,7,8,9,10]
+# square = list(map(lambda x: x*x, num))
+
+# print(square)
+
+
+#
+
+def square(a):
+    return a*a
+
+def cube(a):
+    return a*a*a
+
+
+func = [square,cube]
 num = [1,2,3,4,5,6,7,8,9,10]
-square = list(map(squ, num))
 
-print(square)
-
-
-#lambda Function
-num = [1,2,3,4,5,6,7,8,9,10]
-square = list(map(lambda x: x*x, num))
-
-print(square)
+for i in range(5):
+    val = list(map(lambda x: x(i), func))
+    print (val)
