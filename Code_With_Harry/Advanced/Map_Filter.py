ce2e672db08fba6717c4b676d2215a2b6b636970
shankkalra <shankkalra@gmail.com>
2021-09-01 12:38:47 +0530

    Lambda Func

--- Code_With_Harry/Advanced/Map_Filter.py
+++ Code_With_Harry/Advanced/Map_Filter.py
@@ -11,3 +11,17 @@ number = list(map(int, numbers))
 
 print(number)
 
+def squ(a):
+    return a*a
+    
+num = [1,2,3,4,5,6,7,8,9,10]
+square = list(map(squ, num))
+
+print(square)
+
+
+#lambda Function
+num = [1,2,3,4,5,6,7,8,9,10]
+square = list(map(lambda x: x*x, num))
+
+print(square)
