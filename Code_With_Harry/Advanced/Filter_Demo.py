c5cbe910459c03a634fe9a825d15ca37cfd20bc9
shankkalra <shankkalra@gmail.com>
2021-09-01 12:50:39 +0530

    Reduce Demo

--- Code_With_Harry/Advanced/Filter_Demo.py
+++ Code_With_Harry/Advanced/Filter_Demo.py
@@ -1,3 +1,4 @@
+#Filter
 list_1 = [1, 2, 3, 4, 5, 6,7,8,9]
 
 def is_Greater(num):
@@ -7,3 +8,13 @@ gr_than_5 = filter(is_Greater,list_1)
 
 print (gr_than_5)
 
+
+#Reduce
+
+from functools import reduce
+
+list_2 = [1, 2, 3, 4, 5]
+
+num = reduce(lambda x,y:x*y,list_2)
+
+print (num)
