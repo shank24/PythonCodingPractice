af7891edf33d413cbd6f0536dd821e85c05c3f6c
shankkalra <shankkalra@gmail.com>
2021-09-05 20:33:40 +0530

    Decorator1

--- Code_With_Harry/Advanced/Decorator.py
+++ Code_With_Harry/Advanced/Decorator.py
@@ -1,6 +1,17 @@
-def func1():
-    print("Func1")
+# def func1():
+#     print("Func1")
 
-func2=func1
-del func1
-func2()
+# func2=func1
+# del func1
+# func2()
+
+#Returning function via function
+ 
+def funret(num):
+    if num==0:
+        return print
+    if num==1:
+        return sum 
+    
+a = funret(1)
+print(a)
