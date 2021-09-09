653bc91368a8334a7a1854ac8e2a9af65b8603ba
shankkalra <shankkalra@gmail.com>
2021-09-09 11:38:47 +0530

    Init Demo

--- Code_With_Harry/OOPS/Init_Demo.py
+++ Code_With_Harry/OOPS/Init_Demo.py
@@ -1,19 +1,20 @@
 class Employee:
     leaves = 9
+    def __init__(self,aname,asalary,arole):
+        self.name = aname
+        self.salary = asalary
+        self.role = arole
+
+#name is instance variable, whereas aname is argument.
+
+
+    
 
     def printDetails(self):
         return self.name, self.salary, self.role
 
-harry = Employee()
-rohan = Employee()
-
-harry.name = "Shank"
-harry.salary = 234
-harry.role = "Coder"
-
-rohan.name = "Rohan"
-rohan.salary = 23434
-rohan.role = "Ultra Coder"
+harry = Employee("Harry", 455, "Coder")
+rohan = Employee("Rohan", 466, "Ultra Coder")
 
 print(harry.printDetails())
 
