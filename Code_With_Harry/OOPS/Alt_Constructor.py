4c81fede6ba81439ba5b8010d38d2fc0305c356a
shankkalra <shankkalra@gmail.com>
2021-09-13 22:49:49 +0530

    Alt_Cons

---
+++ Code_With_Harry/OOPS/Alt_Constructor.py
@@ -0,0 +1,36 @@
+class Employee:
+    #Class Variable
+    leaves = 9
+    def __init__(self,aname,asalary,arole):
+        self.name = aname
+        self.salary = asalary
+        self.role = arole
+
+#name is instance variable, whereas aname is argument.
+    def printDetails(self):
+        return self.name, self.salary, self.role
+
+    @classmethod
+    def set_Leaves(cls,newleaves):
+        cls.leaves = newleaves
+    
+    @classmethod
+    def from_str(cls,string):
+        # params = string.split("-")
+        # return cls(params[0],params[1],params[2])
+        return cls(*string.split("-"))
+
+
+harry = Employee("Harry", 455, "Coder")
+rohan = Employee("Rohan", 466, "Ultra Coder")
+cherry = Employee.from_str("Cherry-345-Student")
+
+
+Employee.set_Leaves(33)
+
+print(harry.leaves)
+print(harry.printDetails())
+
+print(rohan.printDetails())
+
+print(cherry.printDetails())
