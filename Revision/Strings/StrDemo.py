86338065592c87cb9ff0220febbb47870de0075e
shankkalra <shankkalra@gmail.com>
2023-01-23 22:01:30 -0400

    String

---
+++ Revision/Strings/StrDemo.py
@@ -0,0 +1,18 @@
+s="You are awesome"
+s1="""You are the
+creator of your
+destiny"""
+
+print(s*2)
+
+print(len(s))
+
+#slicing
+print(s[0:3])
+print(s[0:])
+print(s[:10])
+print(s[-4:-1])
+
+print(s[0:15:1])
+#Reverse a String
+print(s[15::-1])
