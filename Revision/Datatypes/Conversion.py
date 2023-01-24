98bfc197da60e7326ac26028ce344442eaf4bf67
shankkalra <shankkalra@gmail.com>
2023-01-24 10:28:51 -0400

    Data

---
+++ Revision/Datatypes/Conversion.py
@@ -0,0 +1,16 @@
+#Mutable - Ordered
+lst=[1,2,3,4]
+print(type(lst))
+tup=tuple(lst)
+
+print(tup)
+print(type(tup))
+
+set={1,2,3,1,2,4}
+print(type(set))
+print(set)
+tup1=tuple(set)
+print(tup1)
+
+dict={'1':122,'2':123,'3':'124'}
+print(dict)
---
+++ Revision/ListPrograms/square.py
@@ -0,0 +1,5 @@
+sq=[]
+
+for i in range(10):
+    print(i**2)
+
