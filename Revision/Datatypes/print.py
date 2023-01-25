19bdebe0cb0177d6641aef928cd7d01f6cc96a78
shankkalra <shankkalra@gmail.com>
2023-01-25 09:40:47 -0400

    Most Occured Alpha

--- Revision/Datatypes/print.py
+++ Revision/Datatypes/print.py
@@ -1,13 +1,14 @@
-
 print('Hello'*3)
 print('All the power\n is with in you')
 
-a,b=10,20
+a,b = 10,20
 print(a,b)
 
-name='john'
-marks=98
+name = 'john'
+marks = 98
 
 print('Name is , Marks are ',(name,marks))
 
-print("Name is {}, Marks are {}".format(name, marks))
+print("Name is {0}, Marks are {1}".format(name, marks))
+
+
---
+++ Revision/Datatypes/strip.py
@@ -0,0 +1,8 @@
+lst = [int(x) for x in input("Enter 3 numbers sep by space: ").split(',')]
+print(lst)
+
+# txt = "apple#banana#cherry#orange".split("#")
+# print(txt)
+
+# values = input("Input some comma seperated numbers : ")
+# list = values.split(",")
